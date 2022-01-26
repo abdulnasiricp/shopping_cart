6314ea4b2198057748baa026596cfb181acfead2
FlutLab.io <>
2022-01-26 17:09:54 +0000

    Autosave

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -1,23 +1,29 @@
-// import 'package:sqflite/sqflite.dart';
-// import 'package:path_provider/path_provider.dart';
-// import 'dart:io' as io;
-// import 'package:path/path.dart';
-// import 'package:shoppingcart/cart_model.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io' as io;
+import 'package:path/path.dart';
+import 'package:shoppingcart/cart_model.dart';
 
-// class DBHelper {
-//   static Database? _db;
-//   Future<Database?> get db async {
-//     if (_db != null) {
-//       return _db;
-//     }
-//     _db = await initDatabase();
-//   }
+class DBHelper {
+  static Database? _db;
+  Future<Database?> get db async {
+    if (_db != null) {
+      return _db;
+    }
+    _db = await initDatabase();
+  }
 
-//   initDatabase() async {
-//     io.Directory documentDirectory = await getApplicationDocumentsDirectory();
-//     String path = join(documentDirectory.path, 'cart_db');
-//     _onCreate(Database db, int version) async {
-//       await db.execute('CREATE TABLE cart (id INTEGER PRIMARY KEY , productId VARCHAR UNIQUE,productName TEXT,initialPrice INTEGER, productPrice INTEGER , quantity INTEGER, unitTag TEXT , image TEXT )');
-//     }
-//   }
-// }
+  initDatabase() async {
+    io.Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, 'cart_db');
+    _onCreate(Database db, int version) async {
+      await db.execute('CREATE TABLE cart (id INTEGER PRIMARY KEY , productId VARCHAR UNIQUE,productName TEXT,initialPrice INTEGER, productPrice INTEGER , quantity INTEGER, unitTag TEXT , image TEXT )');
+    }
+  }
+
+  Future<Cart> insert(Cart cart) async {
+    var dbClient = await db;
+    await dbClient!.insert('cart', cart.toMap());
+    return cart;
+  }
+}
