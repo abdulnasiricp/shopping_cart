2084967e4e62c7863b3a5f7a70fbd1718695d140
FlutLab.io <>
2022-01-26 06:22:47 +0000

    Autosave

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -9,21 +9,36 @@ class _ProductListScreenState extends State<ProductListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text('product List'),
-      centerTitle: true,
-      actions: [
-        Center(
-          child: Badge(
-            badgeContent: Text('0', style: TextStyle(color: Colors.white)),
-            animationDuration: Duration(milliseconds: 300),
-            child: Icon(Icons.shopping_bag_outlined),
+      appBar: AppBar(
+        title: Text('product List'),
+        centerTitle: true,
+        actions: [
+          Center(
+            child: Badge(
+              badgeContent: Text('0', style: TextStyle(color: Colors.white)),
+              animationDuration: Duration(milliseconds: 300),
+              child: Icon(Icons.shopping_bag_outlined),
+            ),
           ),
-        ),
-        SizedBox(
-          width: 20.0,
-        )
-      ],
-    ));
+          SizedBox(
+            width: 20.0,
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                    child: Column(
+                  children: [],
+                ));
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
