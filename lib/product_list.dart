a59248f4a1b4f117a539c8b118170120f9f202a0
FlutLab.io <>
2022-01-26 04:51:07 +0000

    Autosave

---
+++ lib/product_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProductListScreen extends StatefulWidget {
+  _ProductListScreenState createState() => _ProductListScreenState();
+}
+
+class _ProductListScreenState extends State<ProductListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
